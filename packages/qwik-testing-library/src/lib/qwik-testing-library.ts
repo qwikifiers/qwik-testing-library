69df0583a5c0b86dd745ead99d4c7ad24f871365
Matan Borenkraout <matanbobi@gmail.com>
2023-02-26 17:57:23 +0200

    feat: cleanup and render

--- packages/qwik-testing-library/src/lib/qwik-testing-library.ts
+++ packages/qwik-testing-library/src/lib/qwik-testing-library.ts
@@ -1,3 +1,74 @@
+import { render as qwikRender } from '@builder.io/qwik';
+import {
+  Options,
+  Result,
+  Ui,
+} from 'packages/qwik-testing-library/src/lib/types';
+
 export function doSomething(): string {
   return 'something';
 }
+
+/* istanbul ignore next */
+if (!process.env.QTL_SKIP_AUTO_CLEANUP) {
+  if (typeof afterEach === 'function') {
+    afterEach(async () => {
+      cleanup();
+    });
+  }
+}
+
+const mountedContainers = new Set<HTMLElement>();
+
+async function render(ui: Ui, options: Options = {}): Result {
+  let {
+    container,
+    baseElement = container,
+    queries,
+    wrapper,
+    ...qwikRenderOptions
+  } = options;
+
+  if (!baseElement) {
+    // Default to document.body instead of documentElement to avoid output of potentially-large
+    // head elements (such as JSS style blocks) in debug output.
+    baseElement = document.body;
+  }
+
+  if (!container) {
+    container = baseElement.appendChild(document.createElement('div'));
+  }
+
+  await qwikRender(container, ui, qwikRenderOptions);
+
+  mountedContainers.add(container!);
+
+  // const queryHelpers = getQueriesForElement(container, queries);
+
+  return {
+    asFragment: () => container?.innerHTML as string,
+    container,
+    baseElement,
+    // debug: (el = baseElement, maxLength, options) =>
+    //   Array.isArray(el)
+    //     ? el.forEach((e) => console.log(prettyDOM(e, maxLength, options)))
+    //     : console.log(prettyDOM(el, maxLength, options)),
+    // unmount: dispose,
+    // ...queryHelpers,
+  } as Result;
+}
+
+function cleanupAtContainer(container: HTMLElement) {
+  if (container?.parentNode === document.body) {
+    document.body.removeChild(container);
+  }
+
+  mountedContainers.delete(container);
+}
+
+function cleanup() {
+  mountedContainers.forEach(cleanupAtContainer);
+}
+
+export * from '@testing-library/dom';
+export { cleanup, render };
---
+++ packages/qwik-testing-library/src/lib/types.ts
@@ -0,0 +1,31 @@
+import { JSX } from '@builder.io/qwik/jsx-runtime';
+import { queries } from '@testing-library/dom';
+import type {
+  Queries,
+  BoundFunctions,
+  prettyFormat,
+} from '@testing-library/dom';
+import { Component, RenderOptions } from '@builder.io/qwik';
+
+export type Ui = () => JSX.Element;
+
+export interface Options extends RenderOptions {
+  container?: HTMLElement;
+  baseElement?: HTMLElement;
+  queries?: Queries & typeof queries;
+  wrapper?: Component<{ children: JSX.Element }>;
+}
+
+export type DebugFn = (
+  baseElement?: HTMLElement | HTMLElement[],
+  maxLength?: number,
+  options?: prettyFormat.OptionsReceived
+) => void;
+
+export type Result = BoundFunctions<typeof queries> & {
+  asFragment: () => string;
+  container: HTMLElement;
+  baseElement: HTMLElement;
+  debug: DebugFn;
+  unmount: () => void;
+};
